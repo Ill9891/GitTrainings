46e9808c25c7ff2245fe8f00f7794cfb696faf84
illia_shapovalov <ilya.shapovalov.1989@gmail.com>
2024-02-01 19:03:28 +0200

    Changed string message in Console.WriteLine

--- GitTrainings/Program.cs
+++ GitTrainings/Program.cs
@@ -6,7 +6,7 @@ namespace GitTrainings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("GIT Trainings!");
         }
     }
 }
